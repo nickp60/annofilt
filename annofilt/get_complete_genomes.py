b131a1b2856fb2e539f6a87420aca29482f5aa92
Nick Waters <nickp60@gmail.com>
2019-04-19 13:38:54 +0100

    add checks to get_compete_genomes

--- annofilt/get_complete_genomes.py
+++ annofilt/get_complete_genomes.py
@@ -6,37 +6,27 @@
 
 import os
 import sys
-import glob
-import re
-import datetime
 import subprocess
 import argparse
-import logging
 import shutil
 import pandas as pd
-import multiprocessing
-import copy
 import urllib.request
 
 
 from . import shared_methods as sm
 
-from argparse import Namespace
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
 
-
-def get_args(): #pragma nocover
+def get_args():  # pragma nocover
     parser = argparse.ArgumentParser(
-        description="Given a genus and species, create a pangenome using Roary from complete genomes of the organism",
+        description="Given a genus and species, download complete " +
+        "genomes from NCBI",
         add_help=False)
     parser.add_argument(
         "-g", "--genus",
-        help="genus")
+        help="genus", required=True)
     parser.add_argument(
         "-s", "--species",
-        help="genus")
+        help="genus", required=True)
     parser.add_argument(
         "-n", "--number_of_strains",
         type=int,
@@ -51,6 +41,19 @@ def get_args(): #pragma nocover
         dest="assembly_summary",
         help="Path to assembly_summary.txt from NCBI; if not present, " +
         "will be downloaded")
+    optional.add_argument(
+        "--max_contigs",
+        dest="max_contigs",
+        default=10,
+        help="Maximum number of contigs tolerated. Many fragmented " +
+        " genome assemblies are categorized as 'complete' by ncbi; " +
+        "so if you expect at most 1 chromosome and 10 plasmids, " +
+        "set --max_contigs to 11. default: %(default)s")
+    optional.add_argument(
+        "--max_strains",
+        dest="max_strains",
+        help="how many genomes to try to download to " +
+        " reach the --number_of_strains")
     optional.add_argument(
         "-v",
         "--verbosity",
@@ -73,57 +76,54 @@ def check_exes():
         if shutil.which(exe) is None:
             raise ValueError("%s executable not found" % exe)
 
+
 def get_or_check_assembly_metadata(args, logger):
     if args.assembly_summary is not None:
         if not os.path.isfile(args.assembly_summary):
-            raise ValueError("Assembly file %s invalid" % args.assembly_summary)
+            raise ValueError("Metadata file %s invalid" %
+                             args.assembly_summary)
         else:
-            return args.ssembly_summary
+            return args.assembly_summary
     else:
         new_path = os.path.join(".", "assembly_summary.txt")
         if not os.path.isfile(new_path):
             logger.info("downloading assembly_summary.txt")
-            urllib.request.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt", new_path)
+            urllib.request.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/" +
+                                       "genomes/genbank/bacteria/" +
+                                       "assembly_summary.txt", new_path)
         return new_path
+
 
 def filter_assembly_metadata(args, logger):
     names = "assembly_accession	bioproject	biosample	wgs_master	refseq_category	taxid	species_taxid	organism_name	infraspecific_name	isolate	version_status	assembly_level	release_type	genome_rep	seq_rel_date	asm_name	submitter	gbrs_paired_asm	paired_asm_comp	ftp_path	excluded_from_refseq	relation_to_type_material".split("\t")
-    d = pd.read_csv(args.assembly_summary, sep="\t", skiprows=(0,1),header=(0),names=names)
-    print(d.shape)
-    d = d[d.genome_rep=="Full"]
-    print(d.shape)
-    d = d[(d.assembly_level == "Contig") | (d.assembly_level == "Scaffold")]
-    print(d.shape)
+    d = pd.read_csv(args.assembly_summary, sep="\t",
+                    skiprows=(0, 1), header=(0), names=names)
+    # print(d.shape)
+    d = d[d.genome_rep == "Full"]
+    # print(d.shape)
+    d = d[(d.assembly_level == "Complete Genome") |
+          (d.assembly_level == "Chromosome")]
+    # print(d.shape)
     d = d[(d.excluded_from_refseq.notnull())]
-    print(d.shape)
+    # print(d.shape)
     qname = args.genus + " " + args.species
-    print(qname)
+    # print(qname)
     d = d[d.organism_name.str.startswith(qname)]
-    print(d.shape)
-    print(d.genome_rep.head(3))
+    # print(d.shape)
+    # print(d.genome_rep.head(3))
     return d
 
 
-def get_n_genomes(d, genomes_dir, n, logger, seed=12345):
-    d = d.sample(n, random_state=seed)
-    print(str(d.head(1).ftp_path))
-    for path in d.ftp_path:
-        outpath = os.path.join(genomes_dir, os.path.basename(path) + ".fna.gz")
-        if not os.path.isfile(outpath):
-            cmd = str("wget {0}/{1}_genomic.fna.gz -O {2}").format(
-                path, os.path.basename(path), outpath)
-            print(cmd)
-            subprocess.run(cmd, shell=sys.platform != "win32",
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE, check=True)
-    unzip_cmd = "gunzip %s*gz" % genomes_dir
-    subprocess.run(unzip_cmd, shell=sys.platform != "win32",
-                   stdout=subprocess.PIPE,
-                   stderr=subprocess.PIPE, check=True)
-
-
-def run_roary():
-    pass
+def get_genome(path, genomes_dir, logger):
+    outpath = os.path.join(genomes_dir, os.path.basename(path) + ".fna.gz")
+    if not os.path.isfile(outpath):
+        cmd = str("wget {0}/{1}_genomic.fna.gz -O {2}").format(
+            path, os.path.basename(path), outpath)
+        logger.debug("Getting assembly " + path)
+        subprocess.run(cmd, shell=sys.platform != "win32",
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, check=True)
+    return outpath
 
 
 def main(args=None, logger=None):
@@ -135,8 +135,7 @@ def main(args=None, logger=None):
         sys.stderr.write("creating output directory %s\n" % output_root)
         os.makedirs(output_root)
     else:
-        sys.stderr.write("Output Directory already exists!\n")
-        sys.exit(1)
+        sys.stderr.write("WARNING! Using existing output directory!\n")
     check_exes()
     if logger is None:
         logger = sm.set_up_logging(
@@ -146,15 +145,56 @@ def main(args=None, logger=None):
     logger.debug("All settings used:")
     for k, v in sorted(vars(args).items()):
         logger.debug("{0}: {1}".format(k, v))
-    date = str(datetime.datetime.now().strftime('%Y%m%d'))
+    logger.debug("sorting out the assembly_metadata.txt file")
     args.assembly_summary = get_or_check_assembly_metadata(args, logger)
+    logger.debug("filtering assembly metadata")
     species_df = filter_assembly_metadata(args, logger)
     genomes_dir = os.path.join(output_root, "")
-    get_n_genomes(
-        d=species_df, genomes_dir=genomes_dir,
-        n=args.number_of_strains, logger=logger, seed=12345)
-    logger.debug("Done!")
+    not_enough_strains = True
+    not_reached_max_tries = True
+    counter = 0
+    n_successful = 0
+    # shuffle the order of the strains we have left after filtering
+    species_df = species_df.sample(
+        frac=1, random_state=12345).reset_index(drop=True)
+    logger.debug("Attempting to download strains")
+    if species_df.shape[0] < args.number_of_strains:
+        logger.warning("Only %i elegible strains available!" %
+                       species_df.shape[0])
+        args.number_of_strains = species_df.shape[0]
+    while not_enough_strains and not_reached_max_tries:
+        logger.debug("getting strain %i of %i" %
+                     (n_successful+1, args.number_of_strains))
+        this_path = get_genome(
+            path=species_df.ftp_path[counter], genomes_dir=genomes_dir,
+            logger=logger)
+        logger.debug("    unzipping %s" % this_path)
+        # -o overwrites
+        unzip_cmd = "gunzip -o %s" % this_path
 
+        subprocess.run(unzip_cmd, shell=sys.platform != "win32",
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, check=True)
+
+        logger.debug("    checking number of contigs")
+        grep_cmd = "grep '>' %s" % this_path
+        result = subprocess.run(grep_cmd, shell=sys.platform != "win32",
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, check=True)
+        n_contigs = len(result.stdout.decode("utf-8").split("\n"))
+        if n_contigs > args.max_contigs:
+            logger.info("rejecting %s: %i contigs" % (this_path, n_contigs))
+            os.remove(this_path)
+        else:
+            n_successful = n_successful + 1
+        # status update
+        if n_successful == args.number_of_strains:
+            not_enough_strains = False
+        if counter == args.max_strains:
+            not_reached_max_tries = False
+        counter = counter + 1
+    logger.debug("Saved {} strains!".format(n_successful))
+    logger.debug("Done!")
 
 
 if __name__ == "__main__":
